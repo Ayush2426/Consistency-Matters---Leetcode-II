7099ad100c8318e16788e5f03fcff5d4095536cb
Ayush Verma <av874559@gmail.com>
2025-11-07 23:16:01 +0530

    11th code done

---
+++ Day 1/ValidPalindrome.java
@@ -0,0 +1,22 @@
+public class ValidPalindrome {
+    public boolean checkPalindrome(String s){
+        s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) >= 97 && s.charAt(i) <= 121 || s.charAt(i) >= 48 && s.charAt(i) <= 57){
+                sb.append(s.charAt(i));
+            }
+        }
+        for(int i = 0; i < sb.length() / 2; i++){
+            if(sb.charAt(i) != sb.charAt(sb.length() - 1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        ValidPalindrome vp = new ValidPalindrome();
+        vp.checkPalindrome("race a car");
+    }
+}
