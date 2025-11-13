2df13e8885525fe793425f4b68e8cd952439ac64
Ayush Verma <av874559@gmail.com>
2025-11-13 23:23:49 +0530

    15th code done

---
+++ Day 1/MidLinkedList.java
@@ -0,0 +1,16 @@
+// public class MidLinkedList {
+//     public ListNode checkMidLL(ListNode head){
+//         ListNode slow = head;
+//         ListNode fast = head;
+
+//         while(fast != null && fast.next != null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+//         return slow;
+//     }
+//     public static void main(String[] args) {
+//         MidLinkedList mll = new MidLinkedList();
+//         mll.checkMidLL(head);
+//     }
+// }
