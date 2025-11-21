1121d22b5a85311f252e0201da0554f1b550f7af
Ayush Verma <av874559@gmail.com>
2025-11-21 23:52:47 +0530

    22nd code done

---
+++ Day 1/InvertBinaryTree.java
@@ -0,0 +1,20 @@
+
+// import javax.swing.tree.TreeNode;
+
+// public class InvertBinaryTree {
+//     public TreeNode invertTree(TreeNode root){
+//         if(root == null) return root;
+        
+//         TreeNode left = invertTree(root.left);
+//         TreeNode right = invertTree(root.right);
+
+//         root.left = right;
+//         root.right = left;
+
+//         return root;
+//     }
+//     public static void main(String[] args) {
+//         InvertBinaryTree ibt = new InvertBinaryTree();
+//         ibt.invertTree(null);
+//     }
+// }
