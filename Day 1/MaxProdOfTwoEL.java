1f8b090055e0ccb00b4edd84d11868564334ca1c
Ayush Verma <av874559@gmail.com>
2025-12-06 23:04:34 +0530

    32nd code done

---
+++ Day 1/MaxProdOfTwoEL.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class MaxProdOfTwoEL{
+    public int findMaxProd(int[] nums){
+        Arrays.sort(nums);
+        int firstEl = nums[nums.length - 1];
+        int secEl = nums[nums.length - 2];
+        int MaxProd = firstEl * secEl;
+        return MaxProd;
+    }
+    public static void main(String[] args) {
+        MaxProdOfTwoEL mpotel = new MaxProdOfTwoEL();
+        mpotel.findMaxProd(new int[]{3, 4, 2, 5, 7, 8});
+    }
+}
