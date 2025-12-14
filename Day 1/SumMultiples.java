4a155af0dfa1011da95bdd015fa1856a31361c9a
Ayush Verma <av874559@gmail.com>
2025-12-14 23:56:12 +0530

    Day 40th code done

--- Day 1/SumMultiples.java
+++ Day 1/SumMultiples.java
@@ -1,7 +1,7 @@
 public class SumMultiples {
     public int findSumMultiples(int n){
         int sum = 0;
-        for(int i = 1; i < n; i++){
+        for(int i = 1; i <= n; i++){
             if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
                 sum += i;
             }
