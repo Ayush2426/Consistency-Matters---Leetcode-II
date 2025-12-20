b29eb10845709eab7805d5fd682f343a826ebccb
Ayush Verma <av874559@gmail.com>
2025-12-20 23:30:57 +0530

    45th code done

---
+++ Day 1/IsSubsequence.java
@@ -0,0 +1,21 @@
+public class IsSubsequence {
+    public boolean  checkIfSubSequence(String s, String t){
+        if(s == null || s.equals("")){
+            return true;
+        }
+        int index = 0;
+        for(int i = 0; i < t.length(); i++){
+            if(s.charAt(index) == t.charAt(i)){
+                index++;
+            }
+            if(index == s.length()){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        IsSubsequence is = new IsSubsequence();
+        is.checkIfSubSequence("abc", "abcde");
+    }
+}
