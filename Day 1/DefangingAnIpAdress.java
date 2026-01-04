0f323efd6a2fa21a213c452dde0e21dd466f4cff
Ayush Verma <av874559@gmail.com>
2026-01-04 22:44:07 +0530

    54th code done

---
+++ Day 1/DefangingAnIpAdress.java
@@ -0,0 +1,18 @@
+public class DefangingAnIpAdress {
+    public String FormingIpAdress(String s){
+        StringBuilder sb = new StringBuilder();
+        char arrAdd[] = s.toCharArray();
+        for(char ch : arrAdd){
+            if(ch == '.'){
+                sb.append("[.]");
+            }else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        DefangingAnIpAdress dAdress = new DefangingAnIpAdress();
+        dAdress.FormingIpAdress("122.322.333.2");
+    }
+}
